864e23f52f1642b42ba920f7da1faac32e23a551
Naledi <nthabimasita@gmail.com>
2023-02-16 13:37:11 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 /**
- *  main - Entry
- * Return: Always 0
+ *  main - Entry point
+ *
+ *  Description: 'Size is not grandeur, and territory does not make a nation'
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
