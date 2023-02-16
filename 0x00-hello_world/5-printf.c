2ea0c4af3ba1e1daaca3a9fa584157d6d8b9ba69
Naledi <nthabimasita@gmail.com>
2023-02-16 13:15:31 -0800

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
-int main block {
-	//print()
-	printf"(with proper grammar, but the outcome is a piece of art,");
+/**
+ * main - Entry
+ * Return: Alway 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
