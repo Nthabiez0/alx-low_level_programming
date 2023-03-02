700cdb95d30d072900c5f3521f033f558e5152bd
Naledi <nthabimasita@gmail.com>
2023-03-01 23:44:38 -0800

    2 print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
