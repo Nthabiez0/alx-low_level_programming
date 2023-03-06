0ccadfc65bb4dcd347f9d829ee528450551f67ff
Naledi <nthabimasita@gmail.com>
2023-03-06 10:49:06 -0800

    8-print_diagsums

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * print_chessboard - prints the chessboard
+ *
+ * @a: the row of the array
+ *
+ * Return: nothing
+ */
+
+void print_diagsums(int *a, int size)
+{
+int i;
+int sum1 = 0;
+int sum2 = 0;
+for (i = 0; i < size; i++)
+{
+sum1 += matrix[i * size + i];
+sum2 += matrix[i * size + size - i - 1];
+}
+printf("%d %d\n", sum1, sum2);
+}
+
+int main(void)
+{
+int matrix[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+print_diagsums(&matrix[0][0], 3);
+return (0);
+}
