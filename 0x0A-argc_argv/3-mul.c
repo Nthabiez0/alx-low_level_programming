e95c93e5c5cc3611b4f1722c361a961bb6cf8e51
Naledi <nthabimasita@gmail.com>
2023-03-10 09:30:59 -0800

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * main - multiplies two numbers
  * @argc: number of arguments
@@ -9,36 +11,25 @@
 int main(int argc, char *argv[])
 
 {
+int result, num1, num2;
 
-	int result, num1, num2;
+if (argc < 3 || argc > 3)
+{
 
+printf("Error\n");
 
+return (1);
+}
 
-	if (argc < 3 || argc > 3)
+num1 = _atoi(argv[1]);
 
-	{
+num2 = _atoi(argv[2]);
 
-		printf("Error\n");
+result = num1 *num2;
 
-		return (1);
+printf("%d\n", result);
 
-	}
-
-
-
-	num1 = _atoi(argv[1]);
-
-	num2 = _atoi(argv[2]);
-
-	result = num1 * num2;
-
-
-
-	printf("%d\n", result);
-
-
-
-	return (0);
+return (0);
 
 }
 
